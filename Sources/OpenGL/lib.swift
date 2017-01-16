07cf8517815fb92fcfd86706a911c4b2fa7e3aed
Naithar <devias.naith@gmail.com>
2017-01-16 13:20:50 +0300

    comment out

--- Sources/OpenGL/lib.swift
+++ Sources/OpenGL/lib.swift
@@ -1,8 +1,8 @@
 import COpenGLOSX
 
 //https://www.opengl.org/sdk/docs/man/
-print("Hello, world!")
-
+//print("Hello, world!")
+/*
 glDepthMask(GLboolean(GL_FALSE))
 glClear(GLbitfield(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT))
 glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_WRAP_S), GLint(GL_MIRRORED_REPEAT))
@@ -127,4 +127,4 @@ gl.end()
 gl.begin {
     gl.clear(mask: [.color, .buffer])
     gl.color()
-}
+}*/
